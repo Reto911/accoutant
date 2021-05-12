509f5851b1b1496d0cd787bb0764feb44c15e900
Shiroan <shiroan@qq.com>
2021-05-12 22:44:47 +0800

    FUCK

--- abandon/errOpt.js
+++ abandon/errOpt.js
@@ -1,28 +1,28 @@
-function date() {
-    return {
-      options: {
-        title: {
-          text: "支出成分"
-        },
-        tooltip: {},
-        legend: {
-          data: [
-            "Food",
-            "Play",
-            "Other"
-          ]
-        },
-        series: [
-          {
-            name: "支出",
-            type: "pie",
-            data: [
-              {value: 720, name: "Food"},
-              {value: 350, name: "Play"},
-              {value: 320, name: "Other"}
-            ]
-          }
+function data() {
+  return {
+    options: {
+      title: {
+        text: "支出成分"
+      },
+      tooltip: {},
+      legend: {
+        data: [
+          "Food",
+          "Play",
+          "Other"
         ]
       },
-    }
+      series: [
+        {
+          name: "支出",
+          type: "pie",
+          data: [
+            {value: 720, name: "Food"},
+            {value: 350, name: "Play"},
+            {value: 320, name: "Other"}
+          ]
+        }
+      ]
+    },
   }
+}
